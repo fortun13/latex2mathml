6b197d8ce387ce620b26588c537101bfe6d4ba35
KrzyHonk <honkiszkrzysztof@gmail.com>
2015-04-01 00:14:09 +0200

    Test for basic symbols classes

--- src/Latex2MathML/Scanner/Tests.hs
+++ src/Latex2MathML/Scanner/Tests.hs
@@ -5,10 +5,89 @@ import Latex2MathML.Utils.Definitions
 import Latex2MathML.Scanner.Main
 
 tests :: Test
-tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2, TestLabel "test3" test3, TestLabel "test4" test4, TestLabel "test5" test5, TestLabel "test6" test6, TestLabel "test7" test7, TestLabel "test8" test8, TestLabel "test9" test9, TestLabel "test10" test10, TestLabel "test11" test11, TestLabel "test12" test12, TestLabel "test13" test13, TestLabel "test14" test14, TestLabel "test15" test15, TestLabel "test16" test16,TestLabel "test17" test17,TestLabel "test18" test18,TestLabel "test19" test19]
+tests = TestList [TestLabel "testGr1" testGr1, TestLabel "testGr2" testGr2, TestLabel "testOth" testOth,
+                  TestLabel "testTri" testTri, TestLabel "testSet" testSet, TestLabel "testDel" testDel,
+                  TestLabel "testBin" testBin, TestLabel "testRel" testRel, TestLabel "test1" test1,
+                  TestLabel "test2" test2, TestLabel "test3" test3, TestLabel "test4" test4,
+                  TestLabel "test5" test5, TestLabel "test6" test6, TestLabel "test7" test7,
+                  TestLabel "test8" test8, TestLabel "test9" test9, TestLabel "test10" test10,
+                  TestLabel "test11" test11, TestLabel "test12" test12, TestLabel "test13" test13,
+                  TestLabel "test14" test14, TestLabel "test15" test15, TestLabel "test16" test16,
+                  TestLabel "test17" test17, TestLabel "test18" test18, TestLabel "test19" test19]
 
 --generateTestList 0 = [(,)]
 --generateTestList n = ("test"++n, )
+
+testGr1 :: Test
+testGr1 = TestCase (assertEqual "Greek symbols, part one"
+    ([CommandBodyless "alpha", CommandBodyless "beta", CommandBodyless "Gamma", CommandBodyless "gamma", CommandBodyless "Delta",
+    CommandBodyless "delta", CommandBodyless "epsilon", CommandBodyless "varepsilon", CommandBodyless "zeta", CommandBodyless "eta",
+    CommandBodyless "Theta", CommandBodyless "theta", CommandBodyless "vartheta", CommandBodyless "iota", CommandBodyless "kappa",
+    CommandBodyless "lambda", CommandBodyless "Lambda", CommandBodyless "mu"], "")
+    (scan "\\alpha \\beta \\Gamma \\gamma \\Delta \\delta \\epsilon \\varepsilon \\zeta \\eta \\Theta \\theta \\vartheta \\iota \\kappa \\lambda \\Lambda \\mu"))
+
+testGr2 :: Test
+testGr2 = TestCase (assertEqual "Greek symbols, part two"
+    ([CommandBodyless "nu", CommandBodyless "Xi", CommandBodyless "xi", CommandBodyless "Pi", CommandBodyless "pi",
+     CommandBodyless "varpi", CommandBodyless "rho", CommandBodyless "varrho", CommandBodyless "Sigma", CommandBodyless "sigma",
+     CommandBodyless "varsigma", CommandBodyless "tau", CommandBodyless "Upsilon", CommandBodyless "upsilon", CommandBodyless "Phi",
+     CommandBodyless "phi", CommandBodyless "varphi", CommandBodyless "chi", CommandBodyless "Psi", CommandBodyless "psi",
+     CommandBodyless "Omega", CommandBodyless "omega", CommandBodyless "omicron"], "")
+    (scan "\\nu \\Xi \\xi \\Pi \\pi \\varpi \\rho \\varrho \\Sigma \\sigma \\varsigma \\tau \\Upsilon \\upsilon \\Phi \\phi \\varphi \\chi \\Psi \\psi \\Omega \\omega \\omicron"))
+
+testOth :: Test
+testOth = TestCase (assertEqual "Other symbols class"
+    ([CommandBodyless "partial", CommandBodyless "imath", CommandBodyless "Re", CommandBodyless "nabla", CommandBodyless "aleph",
+    CommandBodyless "eth", CommandBodyless "jmath", CommandBodyless "Im", CommandBodyless "Box", CommandBodyless "beth",
+    CommandBodyless "hbar", CommandBodyless "ell", CommandBodyless "wp", CommandBodyless "infty", CommandBodyless "gimel"], "")
+    (scan "\\partial \\imath \\Re \\nabla \\aleph \\eth \\jmath \\Im \\Box \\beth \\hbar \\ell \\wp \\infty \\gimel"))
+
+testTri :: Test
+testTri = TestCase (assertEqual "Trigonometric symbols"
+    ([CommandBodyless "sin", CommandBodyless "arcsin", CommandBodyless "sinh", CommandBodyless "sec", CommandBodyless "cos",
+    CommandBodyless "arccos", CommandBodyless "cosh", CommandBodyless "csc", CommandBodyless "tan", CommandBodyless "arctan",
+    CommandBodyless "tanh", CommandBodyless "cot", CommandBodyless "arccot", CommandBodyless "coth"], "")
+    (scan "\\sin \\arcsin \\sinh \\sec \\cos \\arccos \\cosh \\csc \\tan \\arctan \\tanh \\cot \\arccot \\coth"))
+
+testSet :: Test
+testSet = TestCase (assertEqual "Set and logic symbols"
+    ([CommandBodyless "exists", CommandBodyless "nexists", CommandBodyless "forall", CommandBodyless "neg", CommandBodyless "subset",
+    CommandBodyless "supset", CommandBodyless "in", CommandBodyless "notin", CommandBodyless "ni", CommandBodyless "lor",
+    CommandBodyless "rightarrow", CommandBodyless "to", CommandBodyless "leftarrow", CommandBodyless "gets", CommandBodyless "mapsto",
+    CommandBodyless "implies", CommandBodyless "Rightarrow", CommandBodyless "leftrightarrow", CommandBodyless "iff", CommandBodyless "Leftrightarrow",
+    CommandBodyless "top", CommandBodyless "land", CommandBodyless "bot", CommandBodyless "emptyset", CommandBodyless "varnothing"], "")
+    (scan "\\exists \\nexists \\forall \\neg \\subset \\supset \\in \\notin \\ni \\lor \\rightarrow \\to \\leftarrow \\gets \\mapsto \\implies \\Rightarrow \\leftrightarrow \\iff \\Leftrightarrow \\top \\land \\bot \\emptyset \\varnothing"))
+
+testDel :: Test
+testDel = TestCase (assertEqual "Delimiter symbols"
+    ([CommandBodyless "uparrow", CommandBodyless "downarrow", CommandBodyless "|", Operator "}", CommandBodyless "Uparrow",
+    CommandBodyless "Downarrow", Operator "/", CommandBodyless "langle", CommandBodyless "lceil", CommandBodyless "lfloor",
+    CommandBodyless "backslash", CommandBodyless "rangle", CommandBodyless "rceil", CommandBodyless "rfloor"], "")
+    (scan "\\uparrow \\downarrow \\| \\} \\Uparrow \\Downarrow / \\langle \\lceil \\lfloor \\backslash \\rangle \\rceil \\rfloor"))
+
+testBin :: Test
+testBin = TestCase (assertEqual "Binary symbols"
+    ([CommandBodyless "pm", CommandBodyless "cap", CommandBodyless "diamond", CommandBodyless "oplus", CommandBodyless "mp",
+    CommandBodyless "cup", CommandBodyless "ominus", CommandBodyless "times", CommandBodyless "uplus", CommandBodyless "bigtriangledown",
+    CommandBodyless "bigtriangleup", CommandBodyless "otimes", CommandBodyless "div", CommandBodyless "sqcap", CommandBodyless "triangleleft",
+    CommandBodyless "oslash", CommandBodyless "ast", CommandBodyless "sqcup", CommandBodyless "triangleright", CommandBodyless "odot",
+    CommandBodyless "star", CommandBodyless "vee", CommandBodyless "bigcirc", CommandBodyless "circ", CommandBodyless "dagger",
+    CommandBodyless "wedge", CommandBodyless "bullet", CommandBodyless "setminus", CommandBodyless "ddagger", CommandBodyless "cdot",
+    CommandBodyless "wr", CommandBodyless "amalg"], "")
+    (scan "\\pm \\cap \\diamond \\oplus \\mp \\cup \\ominus \\times \\uplus \\bigtriangledown \\bigtriangleup \\otimes \\div \\sqcap \\triangleleft \\oslash \\ast \\sqcup \\triangleright \\odot \\star \\vee \\bigcirc \\circ \\dagger \\wedge \\bullet \\setminus \\ddagger \\cdot \\wr \\amalg"))
+
+testRel :: Test
+testRel = TestCase (assertEqual "Relation symbols"
+    ([CommandBodyless "parallel", CommandBodyless "nparallel", CommandBodyless "leq", CommandBodyless "geq", CommandBodyless "doteq",
+    CommandBodyless "asymp", CommandBodyless "bowtie", CommandBodyless "ll", CommandBodyless "gg", CommandBodyless "equiv",
+    CommandBodyless "vdash", CommandBodyless "dashv", CommandBodyless "subset", CommandBodyless "supset", CommandBodyless "approx",
+    CommandBodyless "in", CommandBodyless "ni", CommandBodyless "subseteq", CommandBodyless "supseteq", CommandBodyless "cong",
+    CommandBodyless "smile", CommandBodyless "frown", CommandBodyless "nsubseteq", CommandBodyless "nsupseteq", CommandBodyless "sqsubset",
+    CommandBodyless "sqsupset", CommandBodyless "simeq", CommandBodyless "models", CommandBodyless "notin", CommandBodyless "sim",
+    CommandBodyless "perp", CommandBodyless "mid", CommandBodyless "sqsubseteq", CommandBodyless "sqsupseteq", CommandBodyless "propto",
+    CommandBodyless "prec", CommandBodyless "succ", CommandBodyless "preceq", CommandBodyless "succeq", CommandBodyless "neq"], "")
+    (scan "\\parallel \\nparallel \\leq \\geq \\doteq \\asymp \\bowtie \\ll \\gg \\equiv \\vdash \\dashv \\subset \\supset \\approx \\in \\ni \\subseteq \\supseteq \\cong \\smile \\frown \\nsubseteq \\nsupseteq \\sqsubset \\sqsupset \\simeq \\models \\notin \\sim \\perp \\mid \\sqsubseteq \\sqsupseteq \\propto \\prec \\succ \\preceq \\succeq \\neq"))
+
 
 test1 :: Test
 test1 = TestCase (assertEqual "Testing simple Sup; expression: k^2"
