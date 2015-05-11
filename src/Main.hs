1cdea7470d836a71a0f89e70c3bfd7122b9c3b14
fortun13 <qba1993@gmail.com>
2015-05-11 11:57:10 +0200

    Proper way of generating file (without lifting and depending on proper input)

--- src/Main.hs
+++ src/Main.hs
@@ -7,24 +7,15 @@ import Latex2MathML.Scanner.Tests
 import Latex2MathML.Parser.Main
 import Latex2MathML.Generator.Main
 import Data.Either
+import Control.Monad.Trans.Either
 
 main :: IO a
-main = do
+main =  do
 --    _ <- runTestTT tests
---    print $ scan "A_{m,n} = \\begin{matrix} a_{1,1} & a_{1,2} & \\cdots & a_{1,n} \\\\ a_{2,1} & a_{2,2} & \\cdots & a_{2,n} \\\\ \\vdots  & \\vdots  & \\ddots & \\vdots  \\\\ a_{m,1} & a_{m,2} \\cdots & a_{m,n} \\end{matrix}" >>= parse
---    print $ scan "\\begin{matrix} asd & fd & gf \\\\ fd & fhd & fhd \\end{matrix}" >>= parse
---    print $ scan "32^{101}_43 ala_{ma}^{kota}" >>= parse
---    print $ scan "\\hat{matrix} \\sqrt{25} \\frac{3}{4}" >>= parse
---    print $ scan "2^long \\sin \\alpha" >>= parse
---    writeFile "output.xhtml" (head $ rights $ [generate $ head $ rights $ [parse $ head $ rights $ [scan "2^long \\sin \\alpha"]]])
-    writeFile "output.xhtml" (head $ rights $ [generate $ head $ rights $ [parse $ head $ rights $ [scan "\\hat{matrix} \\sqrt{25} \\frac{3}{4} \\iint"]]])
-    writeFile "output2.xhtml" (head $ rights $ [generate $ head $ rights $ [parse $ head $ rights $ [scan "\\int^{10}_1 \\int_{11}^{20} \\sum^{10}_{1} \\prod_{1}^{10} \\lim_{x \\to \\infty}^{12} \\exp(-x) = 0"]]])
-    print $ head $ rights $ [parse $ head $ rights $ [scan "\\int^{10}_1 \\int_{11}^{20} \\sum^{10}_{1} \\prod_{1}^{10} \\lim_{x \\to \\infty}^{12} \\exp(-x) = 0"]]
---    writeFile "output.xhtml" (head $ rights $ [generate $ head $ rights $ [parse $ head $ rights $ [scan "32^{101}_43 ala_{ma}^{kota}"]]])
-    -- TODO it's a very primitive way to extract right value from Either
-    -- TODO There is module Data.Either.Unwrap (IntelliJ suggest Data.Either.Extra) withs function fromRight (for a single Either, not list)
-    -- TODO But compiler don't see it - will find out a solution later
-
---    print $ scan "\\sqrt{++}" >>= parse
+    eitherT (\x -> print x) (\x -> saveToFile "output.xhtml" x) (hoistEither $ scan "\\frac{\\alpha}{\\beta}" >>= parse >>= generate)
     exitSuccess
 
+saveToFile :: String -> String -> IO ()
+saveToFile filename content = do
+    writeFile filename content
+    print $ "File " ++ filename ++ " generated!"
